e088ee23a29d1864683cf15e65508ccecd0cf748
MAX <18616297618@163.com>
2016-07-15 09:25:36 +0800

    本地通知
    设置本地通知lotcdnote-创建－设置内容－建立    设置图标右上角红点的数值

---
+++ 远程通知/远程通知/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  远程通知
+//
+//  Created by MAX on 16/7/15.
+//  Copyright © 2016年 MAX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
